7ac6ea2bd15a04b59990da5855dfedadb89ef24c
Salamon <Su1im51n@mail.ru>
2021-08-10 14:24:46 +0300

    add validate email and pass for Login and Register

--- app/src/main/java/com/way/august/AuthViewModel.kt
+++ app/src/main/java/com/way/august/AuthViewModel.kt
@@ -1,19 +1,50 @@
 package com.way.august
 
 import androidx.lifecycle.MutableLiveData
-import com.rengwuxian.materialedittext.MaterialEditText
 
 class AuthViewModel {
 
     val state = MutableLiveData<AuthState>().default(initiaValue = AuthState.DefaultState())
 
     fun login(email: String, pass: String) {
-         state.set(newValue = AuthState.SucceededState())
+        if(!validateEmail(email = email)){
+            state.set(newValue = AuthState.ErrorState<Int>(message = R.string.email_invalid))
+            return
+        }
+        if (!validatePass(pass = pass)){
+            state.set(newValue = AuthState.ErrorState<Int>(message = R.string.auth_validate_faild))
+            return
+        }
+
+        //connect DB
+        state.set(newValue = AuthState.SucceededState())
 
     }
 
+
     fun register(email: String, pass: String, name: String, phone: String) {
-            state.set(newValue = AuthState.SucceededState())
+        if(!validateEmail(email = email)){
+            state.set(newValue = AuthState.ErrorState<Int>(message = R.string.email_invalid))
+            return
+        }
+        if(!validatePass(pass = pass)){
+            state.set(newValue = AuthState.ErrorState<Int>(message = R.string.pass_invalid))
+            return
+        }
+
+        //connect DB
+        state.set(newValue = AuthState.SucceededState())
+
+    }
+
+
+
+    private fun validatePass(pass: String): Boolean {
+        return (pass.length > 5)
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        return email.contains("@")&&email.contains(".")
     }
 
 }
